f1a5f42db9ca404cd0e0db43aa5a9b4b75d449cf
dhirajkafle553 <dhirajkafle553@gmail.com>
2024-02-26 18:43:41 +0545

    Model deployment in progress

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 from model import ml_pipeline 
 
 from typing import Union
-
 from fastapi import FastAPI
 
 
+## Note: FastAPI default provides http://127.0.0.1:8000/docs url to access "Swagger UI" API/Endpoints Documentation.
 
+
+# app means creating a FastAPI instance/object
 app = FastAPI()
+
 
 
 @app.get("/")
 def read_root():
-    return {"me": "47"}
+    return {"ML recommendation model deployment using": "FastAPI"}
 
+
+
+@app.post("/recommendations")
+def request_recommendations(movie: str, rating: int):
+    # pass
+
+    recommendations = ml_pipeline(movie, rating)
+
+    return {"similar movies": recommendations}
